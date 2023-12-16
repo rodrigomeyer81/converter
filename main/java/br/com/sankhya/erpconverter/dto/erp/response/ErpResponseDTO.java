13fa57f87c86ab42a91b4b0128f05b4abc38c567
Rodrigo Meyer <128311039+rodrigomeyer81@users.noreply.github.com>
2023-12-16 13:20:41 -0300

    Init

--- main/java/br/com/sankhya/erpconverter/dto/erp/response/ErpResponseDTO.java
+++ main/java/br/com/sankhya/erpconverter/dto/erp/response/ErpResponseDTO.java
@@ -1,49 +1,52 @@
 package br.com.sankhya.erpconverter.dto.erp.response;
 
+import org.springframework.http.HttpStatus;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
 import br.com.sankhya.erpconverter.exception.MessageError;
 import br.com.sankhya.erpconverter.exception.SankhyaConverserException;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 @Builder
 @Data
 public class ErpResponseDTO {
 
-    private String serviceName;
+	private String serviceName;
 
-    private String status;
+	private String status;
 
-    private HttpStatus httpStatus;
+	private HttpStatus httpStatus;
 
-    private String pendingPrinting;
+	private String pendingPrinting;
 
-    private String transactionId;
+	private String transactionId;
 
-    private ErpResponseBodyDTO responseBody;
+	private ErpResponseBodyDTO responseBody;
 
-    private ErpResponseTsErrorDTO tsError;
+	private ErpResponseTsErrorDTO tsError;
 
-    private String statusMessage;
+	private String statusMessage;
 
+	public static ErpResponseDTO generation(String sson) {
 
-    public static ErpResponseDTO generation(String sson){
+		ErpResponseDTO responseDTO = null;
 
-        ErpResponseDTO responseDTO = null;
+		try {
+			responseDTO = new Gson().fromJson(sson, ErpResponseDTO.class);
 
-        try {
-            responseDTO = new Gson().fromJson(sson, ErpResponseDTO.class);
+		} catch (JsonSyntaxException ex) {
 
-        }catch (JsonSyntaxException ex){
+			throw new SankhyaConverserException(MessageError.MSG_ERR_READ_ERP_REPONSE);
+		}
+		return responseDTO;
+	}
 
-            throw new SankhyaConverserException(MessageError.MSG_ERR_READ_ERP_REPONSE);
-        }
-        return responseDTO;
-    }
+	public String toJson() {
+		return new GsonBuilder().disableHtmlEscaping().create().toJson(this);
+	}
+
 }
-
-
-
-
